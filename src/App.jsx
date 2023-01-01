4fdca7afd68ba88715ab377aa53deee9c5916574
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-01-01 15:22:12 +0300

    use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import AR from "./components/AR"
 import Home from "./components/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Suspense } from "react"
+import { Loader } from "@zappar/zappar-react-three-fiber"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="ar" element={<AR />} />
+        <Route
+          path="ar"
+          element={
+            <Suspense fallback={<Loader />}>
+              <AR />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,10 @@
-import { Suspense, useState } from "react"
 import { Link } from "react-router-dom"
-import AR from "./AR"
-import { Loader } from "@zappar/zappar-react-three-fiber"
 
 const Home = () => {
-  const [arOn, setArOn] = useState(false)
   return (
-    <Suspense fallback={<Loader onLoad={() => console.log("loaded")} />}>
-      {/* <Link to="/ar">Go to AR</Link> */}
-
-      {arOn ? (
-        <AR />
-      ) : (
-        <button className="arToggleBtn" onClick={() => setArOn(true)}>
-          Go to AR
-        </button>
-      )}
-    </Suspense>
+    <>
+      <Link to="/ar">Go to AR</Link>
+    </>
   )
 }
 
