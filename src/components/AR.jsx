d99232878a00086229f3ee9d1c21d693a1969211
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-01-01 18:54:15 +0300

    update sheet images

--- src/components/AR.jsx
+++ src/components/AR.jsx
@@ -8,14 +8,29 @@ import {
 import * as THREE from "three"
 import { useLoader } from "react-three-fiber"
 
-import img from "../assets/transparent.png"
+/* import img1 from "../assets/la-dispute-1.png"
+import img2 from "../assets/la-dispute-2.png"
+import img3 from "../assets/la-dispute-3.png" */
+
+import hammy1 from "../assets/hammy-1.png"
+
 import Spinner from "./Spinner"
 
 const AR = () => {
   const [placementMode, setPlacementMode] = useState(true)
   const [isSpinning, setIsSpinning] = useState(true)
-  const texture = useLoader(THREE.TextureLoader, img)
-  texture.generateMipmaps = false
+
+  /* const texture1 = useLoader(THREE.TextureLoader, img1)
+  texture1.generateMipmaps = false
+
+  const texture2 = useLoader(THREE.TextureLoader, img2)
+  texture2.generateMipmaps = false
+
+  const texture3 = useLoader(THREE.TextureLoader, img3)
+  texture3.generateMipmaps = false */
+
+  const hammyTexture1 = useLoader(THREE.TextureLoader, hammy1)
+  hammyTexture1.generateMipmaps = false
 
   return (
     <>
@@ -31,13 +46,25 @@ const AR = () => {
           placementMode={placementMode}
           placementCameraOffset={[0, 0, -5]}
         >
-          <mesh>
+          {/* <mesh>
             <planeBufferGeometry attach="geometry" args={[2.5, 2.5]} />
-            <meshBasicMaterial attach="material" map={texture} transparent />
+            <meshBasicMaterial attach="material" map={texture1} transparent />
           </mesh>
           <mesh position={[2.5, 0, 0]}>
             <planeBufferGeometry attach="geometry" args={[2.5, 2.5]} />
-            <meshBasicMaterial attach="material" map={texture} transparent />
+            <meshBasicMaterial attach="material" map={texture2} transparent />
+          </mesh>
+          <mesh position={[5, 0, 0]}>
+            <planeBufferGeometry attach="geometry" args={[2.5, 2.5]} />
+            <meshBasicMaterial attach="material" map={texture3} transparent />
+          </mesh> */}
+          <mesh>
+            <planeBufferGeometry attach="geometry" args={[2.5, 2.5]} />
+            <meshBasicMaterial
+              attach="material"
+              map={hammyTexture1}
+              transparent
+            />
           </mesh>
         </InstantTracker>
         <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <Link to="/ar">Go to AR</Link>
-    </>
+    </div>
   )
 }
 
