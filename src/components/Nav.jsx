47a5c6b5793d6e6c4214b016407580b0b9d6dc97
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-01-06 00:05:57 +0300

    add logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import { SettingOutlined } from "@ant-design/icons"
+import logoBlack from "../assets/logo-black.png"
+import logoWhite from "../assets/logo-white.png"
 
 const Nav = ({ isAr, openModal }) => {
   return (
     <nav>
       <Link to="/" className={`${isAr && "arNavHome"}`}>
-        Home
+        <img src={isAr ? logoBlack : logoWhite} alt="logo" />
       </Link>
       <SettingOutlined style={{ fontSize: "2rem" }} onClick={openModal} />
     </nav>
